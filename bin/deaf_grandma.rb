9fcc64228f334a67f44320d5fbd9f529a569dd71
marthaberner <martha@marthaberner.com>
2014-03-09 17:34:55 -0600

    Passed deaf grandma testing

---
+++ bin/deaf_grandma.rb
@@ -0,0 +1,21 @@
+puts "Tell me something."
+answer = gets.chomp
+year = rand (1930..1950)
+
+ if answer != answer.upcase
+  puts "HUH?! SPEAK UP SONNY!"
+else
+  puts "NO, NOT SINCE #{year}!"
+end
+
+
+
+#puts "GRANDMA, HOW OLD ARE YOU?"
+
+#year <= 1950
+#
+#if year = 1930
+ # 10.times puts "NO, NOT SINCE #{year}!"
+  #year += 1
+  #end
+
