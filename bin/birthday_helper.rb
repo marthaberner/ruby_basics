f6c3b0de28453e6934a3ae864f93d5c39d48548f
marthaberner <martha@marthaberner.com>
2014-03-09 20:18:45 -0600

    Added rest of names and birthdays to hashand attempted to access a particular hash

---
+++ bin/birthday_helper.rb
@@ -0,0 +1,4 @@
+birthday_hash = {"Mike" => "May 2, 1987,\n27 years old", "Dean" => "November 23, 1920,\n94 years old", "Kinsey Durham" => "April 1, 1945\n69 years old"}
+
+birthday_hash["Mike"]
+puts["Mike"]
